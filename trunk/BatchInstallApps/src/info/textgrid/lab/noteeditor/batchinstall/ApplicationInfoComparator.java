b7566e6902109287d3b04effc49146880958f66b
Julian.Dabbert <Julian.Dabbert@6cecc7cf-3d11-8543-7be9-9e290bbbb518>
2011-08-03 07:46:37 +0000

    Fixed sorting prob; fixed writeXML prompt icon

---
+++ trunk/BatchInstallApps/src/info/textgrid/lab/noteeditor/batchinstall/ApplicationInfoComparator.java
@@ -0,0 +1,27 @@
+package info.textgrid.lab.noteeditor.batchinstall;
+
+import java.util.Comparator;
+
+import android.content.pm.ApplicationInfo;
+
+/**
+ * Compares two AppInfos for sortability
+ * @author julian
+ *
+ */
+public class ApplicationInfoComparator implements Comparator<ApplicationInfo> {
+
+	@Override
+	public int compare(ApplicationInfo appInfo1, ApplicationInfo appInfo2) {
+		   if (appInfo1.packageName == null && appInfo2.packageName == null) {
+			      return 0;
+			    }
+			    if (appInfo1.packageName == null) {
+			      return 1;
+			    }
+			    if (appInfo2.packageName == null) {
+			      return -1;
+			    }
+			    return (appInfo1.packageName).compareTo(appInfo2.packageName);
+			  }
+}
